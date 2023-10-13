124a4405e5fcc8419a15064bf15e713ea14d9d46
crowdbotics-dev <engineering@crowdbotics.com>
2023-10-13 06:58:41 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cdrer
+admin.site.register(Cdrer)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Cdrer(models.Model):
+    'Generated Model'
+    retyye = models.BigIntegerField()
+    yrueie = models.BigIntegerField()
